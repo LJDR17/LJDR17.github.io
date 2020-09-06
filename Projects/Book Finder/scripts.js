904dec21663dd06810cfdf53a5289bf5a4d65dd7
Luca De Rosa <67323776+LJDR17@users.noreply.github.com>
2020-09-06 13:35:05 +0100

    Update scripts.js

--- Projects/Book Finder/scripts.js
+++ Projects/Book Finder/scripts.js
@@ -1,100 +1,49 @@
-var num = 0;
-var click = 1;
-var varingriedient = 0;
-var vargranny = 0;
-var varbakery = 0;
-var price = 29;
-var price2 = 99;
-var price3 = 499;
-
-//colour of buttons
-setInterval(auto, 100)
-    function auto() {
-    //ingriedient upgrade button
-    if (num > price) {
-     
-        document.getElementById("ingriedient").innerHTML = "Buy ingriedients";
-        document.getElementById("ingriedient").style.backgroundColor = "chocolate";
+function main() {
+    input = document.forms.search.input.value;
+    function validate() {
+        if (input === "") {
+            alert("You can't leave the input blank");
+            return false;
+        }
+        else {
+            return true;
+        }
     }
-    else {
-        document.getElementById("ingriedient").innerHTML = "Not enough money";
-        document.getElementById("ingriedient").style.backgroundColor = "red";
-    }
-    //Granny upgrade button
-    if (num > price2) {
-        document.getElementById("granny").innerHTML = "Upgrade grannies";
-        document.getElementById("granny").style.backgroundColor = "chocolate";
-    }
-    else {
-        document.getElementById("granny").innerHTML = "Not enough money";
-        document.getElementById("granny").style.backgroundColor = "red";
-    }
-    //bakery upgrade button
-    if (num > price3) {
-        document.getElementById("bakery").innerHTML = "Buy bakeries";
-        document.getElementById("bakery").style.backgroundColor = "chocolate";
-    }
-    else {
-        document.getElementById("bakery").innerHTML = "Not enough money";
-        document.getElementById("bakery").style.backgroundColor = "red";
-    }
-}
-
-//reset game
-function reset() {
-    location.reload();
-}
-
-//cookie clicker
-function clickme() { 
-    num += click;
-    document.getElementById("number").innerHTML = num;
+    if (validate()){
+    //books=[];
+   	var url = "";
+   	var image = "";
+    var title = "";
+    var author = "";
+    var publisher = "";
     
-}   
+    $.get("https://www.googleapis.com/books/v1/volumes?q=" + input, function(response) {
+        for(i=0;i<response.items.length;i++){
+            
+            title=$('<h5>' + response.items[i].volumeInfo.title + '</h5>'); 
+            title.appendTo('#books'); 
 
-//ingriedient button
-function ingriedient() {
-   if (num > price) {
-        varingriedient += 1;
-        document.getElementById("numings").innerHTML = "Ingriedients:" + " " + varingriedient;
-        num = num - price;
-        document.getElementById("number").innerHTML = num;
-        click += 2;
-        price += 30;
-        document.getElementById("priceing").innerHTML = "Cost:" + " " + price;
-    }   
-}
+            author=$('<h5> By ' + response.items[i].volumeInfo.authors + '</h5>');
+            author.appendTo('#books');
 
-//granny button
-function granny() {
-    setInterval(auto1, 1000)
-    function auto1() {
-        num += 10;
-        document.getElementById("number").innerHTML = num;
+            publisher=$('<h5> Published by ' + response.items[i].volumeInfo.publisher + '</h5>');
+            publisher.appendTo('#books');
+                
+            image = $('<img><br><a href=' + response.items[i].volumeInfo.infoLink + '</a>');
+            url=response.items[i].volumeInfo.imageLinks.thumbnail
+            image.attr('src', url);
+            image.appendTo('#books');
+        }
+        
+        /*for(i=0;i<response.items.length;i++) {
+            books.push(response.items[i].volumeInfo.title);
+            books.push(response.items[i].volumeInfo.authors);
+            books.push(response.items[i].volumeInfo.imageLinks.thumbnail);
+        }
+        console.log(books)
+        let length = response.items.length;*/
+
+        })
+        
     }
-    if (num > price2) {
-        vargranny += 1;
-        document.getElementById("numgrans").innerHTML = "Grannies:" + " " + vargranny;
-        num = num - price2;
-        document.getElementById("number").innerHTML = num;
-        price2 += 80;
-        document.getElementById("pricegran").innerHTML = "Cost:" + " " + price2;   
-    }  
-}
-
-//bakery button
-function bakery() {
-    setInterval(auto3, 4000)
-    function auto3() {
-        num += 80;
-        document.getElementById("number").innerHTML = num;
-    }
-    if (num > price3) {
-        varbakery += 1;
-        document.getElementById("numbaks").innerHTML = "Bakeries:" + " " + varbakery;
-        num = num - price3;
-        document.getElementById("number").innerHTML = num;
-        price3 += 200;
-        document.getElementById("pricebak").innerHTML = "Cost:" + " " + price3;
-    }  
 }
